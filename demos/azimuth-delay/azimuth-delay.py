6affae84f2e15155bd3796fd5b88a0af2505f137
Jeija <jeija@jeija.net>
2026-02-03 17:19:21 +0100

    Update azimuth-delay demo for new app framework

--- demos/azimuth-delay/azimuth-delay.py
+++ demos/azimuth-delay/azimuth-delay.py
@@ -1,136 +1,166 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import espargos
-import argparse
-from PyQt6 import QtCore
-from PyQt6 import QtQml
-from PyQt6 import QtWidgets
-from matplotlib import colormaps
 import pathlib
 import sys
 
+sys.path.append(str(pathlib.Path(__file__).absolute().parents[2]))
+
+from demos.common import ESPARGOSApplication, ESPARGOSApplicationFlags, ConfigManager
+
+from matplotlib import colormaps
+import numpy as np
+import espargos
+
+import PyQt6.QtCore
 
 BEAMSPACE_OVERSAMPLING = 16
 DELAY_OVERSAMPLING = 10
-DELAY_MIN = -3
-DELAY_MAX = 5
-ANTENNAS_ROWS = 2
-ANTENNAS_COLS = 4
-SUBCARRIERS_LLTF = 53
-SUBCARRIERS_HT20 = 56
-SUBCARRIERS_HT40 = 117
 
 
-class q_image_app(QtWidgets.QApplication):
-    dataChanged = QtCore.pyqtSignal(list)
+class AzimuthDelayApp(ESPARGOSApplication):
+    DEFAULT_CONFIG = {
+        "delay_min": -3,
+        "delay_max": 5,
+    }
+
+    dataChanged = PyQt6.QtCore.pyqtSignal(list)
+    configChanged = PyQt6.QtCore.pyqtSignal()
+    preambleFormatChanged = PyQt6.QtCore.pyqtSignal()
+
     def __init__(self, argv):
-        super().__init__(argv) 
+        super().__init__(argv, flags = {
+            ESPARGOSApplicationFlags.ENABLE_BACKLOG,
+            ESPARGOSApplicationFlags.COMBINED_ARRAY,
+            ESPARGOSApplicationFlags.SINGLE_PREAMBLE_FORMAT
+        })
 
-        parser = argparse.ArgumentParser(description="Demo: Azimuth-Range (single board)")
-        parser.add_argument("host", type=str, help="Host address (IP or hostname) of ESPARGOS controller")
-        parser.add_argument("-b", "--backlog", type = int, default = 1, help = "Number of CSI datapoints to average over in backlog")
-        format_group = parser.add_mutually_exclusive_group()
-        format_group.add_argument("-l", "--lltf", default = False, help = "Use only CSI from L-LTF", action = "store_true")
-        format_group.add_argument("-ht40", "--ht40", default = False, help = "Use only CSI from HT40", action = "store_true")
-        format_group.add_argument("-ht20", "--ht20", default = False, help = "Use only CSI from HT20", action = "store_true")
-        self.args = parser.parse_args()
+        # App-specific configuration
+        self.appconfig = ConfigManager(self.get_initial_config("app"), parent=self)
+        self.appconfig.updateAppState.connect(self.onConfigUpdate)
 
-        # Check if at least one format is selected
-        if not (self.args.lltf or self.args.ht40 or self.args.ht20):
-            print("Error: At least one of --lltf, --ht40 or --ht20 must be selected.")
-            sys.exit(1)
+        # Subscribe to preamble format changes
+        self.genericconfig.updateAppState.connect(self._on_preamble_format_changed)
 
-        if self.args.lltf:
-            self.subcarriers = SUBCARRIERS_LLTF
-        elif self.args.ht20:
-            self.subcarriers = SUBCARRIERS_HT20
-        else:
-            self.subcarriers = SUBCARRIERS_HT40
+        # Initialize pool and backlog
+        self.initialize_pool()
+        self.initComplete.connect(self.onInitComplete)
 
-        self.delay_size = (DELAY_MAX-DELAY_MIN)*DELAY_OVERSAMPLING+1
-        self.angle_size = ANTENNAS_COLS*BEAMSPACE_OVERSAMPLING+1
-        self.pool = espargos.Pool([espargos.Board(self.args.host)])
-        self.pool.start()
-        self.pool.calibrate(duration = 2, per_board=False)
-        fields = ["rssi", "timestamp", "host_timestamp", "mac"]
-        if self.args.lltf:
-            fields.append("lltf")
-        if self.args.ht40:
-            fields.append("ht40")
-        if self.args.ht20:
-            fields.append("ht20")
-        self.backlog = espargos.CSIBacklog(self.pool, size = self.args.backlog, fields = enable)
-        self.backlog.start()
-        self.aboutToQuit.connect(self.onAboutToQuit)
-        self.engine = QtQml.QQmlApplicationEngine()
+        self._delay_size = (self.appconfig.get("delay_max") - self.appconfig.get("delay_min")) * DELAY_OVERSAMPLING + 1
+        self._angle_size = self.n_cols * BEAMSPACE_OVERSAMPLING + 1
+
         self.data = []
-        self.engine.rootContext().setContextProperty("backend", self)
-        self.engine.rootContext().setContextProperty("delay_size", self.delay_size)
-        self.engine.rootContext().setContextProperty("angle_size", self.angle_size)
-        self.engine.rootContext().setContextProperty("Delay_max", DELAY_MAX) 
-        self.engine.rootContext().setContextProperty("Delay_min", DELAY_MIN) 
 
+    def _on_preamble_format_changed(self, newcfg):
+        self.preambleFormatChanged.emit()
+        self.genericconfig.updateAppStateHandled.emit()
 
+    def onInitComplete(self):
+        self.updateDelaySize()
+
+    def onConfigUpdate(self, newconfig):
+        if "delay_min" in newconfig or "delay_max" in newconfig:
+            self.updateDelaySize()
+            self.configChanged.emit()
+        self.appconfig.updateAppStateHandled.emit()
+
+    def updateDelaySize(self):
+        delay_min = self.appconfig.get("delay_min")
+        delay_max = self.appconfig.get("delay_max")
+        self._delay_size = (delay_max - delay_min) * DELAY_OVERSAMPLING + 1
 
     def exec(self):
-        qmlFile = pathlib.Path(__file__).resolve().parent /"azimuth-delay.qml"
-        self.engine.load(qmlFile.as_uri())
+        qml_file = pathlib.Path(__file__).resolve().parent / "azimuth-delay.qml"
+        self.initialize_qml(qml_file, context_props={"appconfig": self.appconfig})
+
         if not self.engine.rootObjects():
             return -1
-        return super().exec()
-    
 
-    def onAboutToQuit(self):
-        self.pool.stop()
-        self.backlog.stop()
-        self.engine.deleteLater()
-          
+        return super().exec()
+
     def colormap(self, data):
-        colormap = (colormaps.get_cmap('viridis'))
-        image_data_list = []
-        norm_data = data/np.max(data) if np.max(data)!=0 else data
-        np_data = np.zeros((data.shape[1],data.shape[0],4), dtype=np.uint8)
-        color_data=colormap(np.transpose(norm_data))
-        np_data = color_data*255
-        self.angle_size = np_data.shape[1]
-        self.delay_size = np_data.shape[0]
-        self.engine.rootContext().setContextProperty("delay_size", self.delay_size)
-        self.engine.rootContext().setContextProperty("angle_size", self.angle_size)
+        colormap = colormaps.get_cmap("viridis")
+        norm_data = data / np.max(data) if np.max(data) != 0 else data
+        color_data = colormap(np.transpose(norm_data))
+        np_data = color_data * 255
+        self._angle_size = np_data.shape[1]
+        self._delay_size = np_data.shape[0]
         image_data_list = np_data.flatten().tolist()
         return image_data_list
-    
-    def get_csi(self):
-        if self.backlog.nonempty():
-            if self.args.lltf:
-                csi = self.backlog.get("lltf")
-                espargos.util.interpolate_lltf_gap(csi)
-            elif self.args.ht20:
-                csi = self.backlog.get("ht20")
-                espargos.util.interpolate_ht20ltf_gap(csi)
-            else:
-                csi = self.backlog.get("ht40")
-                espargos.util.interpolate_ht40ltf_gap(csi)
-            return csi
-        else:
-            return np.zeros((1,1,2,ANTENNAS_COLS,self.subcarriers), dtype=complex)
-        
-    
-    @QtCore.pyqtSlot()
-    def update_data(self):
-            csi=self.get_csi()
-            csi = csi[:,0,...] #only one board used
-            csi = np.sum(csi, axis = 1)  #beamform over both rows of array
-            csi_padded = np.zeros((csi.shape[0], ANTENNAS_COLS*BEAMSPACE_OVERSAMPLING, self.subcarriers*DELAY_OVERSAMPLING), dtype=csi.dtype)
-            csi_padded[:,:csi.shape[1], :csi.shape[2]] = csi
-            csi_padded = np.roll(np.fft.fft(csi_padded, axis=1), (ANTENNAS_COLS*BEAMSPACE_OVERSAMPLING)/2, axis=1) #beamspace
-            csi_padded = np.roll(np.fft.ifft(csi_padded, axis=2), -DELAY_MIN*DELAY_OVERSAMPLING, axis=2) #from frequency to delay domain
-            csi_padded = np.abs(csi_padded)
-            csi_padded = np.sum(csi_padded, axis=0) #sum of all backlog samples
-            self.data = csi_padded[:,:(DELAY_MAX-DELAY_MIN)*DELAY_OVERSAMPLING+1]# only relevant delays
-            self.data = np.append(self.data, self.data[0,:].reshape(1,(DELAY_MAX-DELAY_MIN)*DELAY_OVERSAMPLING+1), axis=0) #beamspace -pi identical to pi
-            self.image_data = self.colormap((self.data))
-            self.dataChanged.emit(self.image_data)
 
-app = q_image_app(sys.argv)
+    def get_subcarrier_count(self):
+        preamble_format = self.genericconfig.get("preamble_format")
+        if preamble_format == "lltf":
+            return espargos.csi.LEGACY_COEFFICIENTS_PER_CHANNEL
+        elif preamble_format == "ht20":
+            return espargos.csi.HT_COEFFICIENTS_PER_CHANNEL
+        else:
+            return espargos.csi.HT_COEFFICIENTS_PER_CHANNEL * 2 + espargos.csi.HT40_GAP_SUBCARRIERS
+
+
+    @PyQt6.QtCore.pyqtSlot()
+    def update_data(self):
+        preamble_format = self.genericconfig.get("preamble_format")
+
+        try:
+            csi = self.backlog.get(preamble_format)
+        except ValueError:
+            print(f"Requested CSI key {preamble_format} not in backlog")
+            return
+
+        # If any value is NaN skip this update (happens if received frame were not of expected type)
+        if np.isnan(csi).any():
+            return
+        
+        # If backlog is empty, skip update
+        if csi.size == 0:
+            return
+
+        if preamble_format == "ht20":
+            espargos.util.interpolate_ht20ltf_gap(csi)
+        elif preamble_format == "ht40":
+            espargos.util.interpolate_ht40ltf_gap(csi)
+
+        # Remove STO from CSI
+        espargos.util.remove_mean_sto(csi)
+
+        delay_min = self.appconfig.get("delay_min")
+        delay_max = self.appconfig.get("delay_max")
+
+        subcarriers = csi.shape[-1]
+
+        # Build combined array CSI
+        csi_largearray = espargos.util.build_combined_array_csi(self.indexing_matrix, csi)
+        # csi_largearray shape: (backlog_depth, n_rows, n_cols, subcarriers)
+
+        # Sum over rows (beamform vertically)
+        csi = np.sum(csi_largearray, axis=1)  # shape: (backlog_depth, n_cols, subcarriers)
+
+        csi_padded = np.zeros((csi.shape[0], self.n_cols * BEAMSPACE_OVERSAMPLING, subcarriers * DELAY_OVERSAMPLING), dtype=csi.dtype)
+        csi_padded[:, :csi.shape[1], :csi.shape[2]] = csi
+        csi_padded = np.roll(np.fft.fft(csi_padded, axis=1), (self.n_cols * BEAMSPACE_OVERSAMPLING) // 2, axis=1)  # beamspace
+        csi_padded = np.roll(np.fft.ifft(csi_padded, axis=2), -delay_min * DELAY_OVERSAMPLING, axis=2)  # from frequency to delay domain
+        csi_padded = np.abs(csi_padded)
+        csi_padded = np.sum(csi_padded, axis=0)  # sum of all backlog samples
+        self.data = csi_padded[:, :(delay_max - delay_min) * DELAY_OVERSAMPLING + 1]  # only relevant delays
+        self.data = np.append(self.data, self.data[0, :].reshape(1, (delay_max - delay_min) * DELAY_OVERSAMPLING + 1), axis=0)  # beamspace -pi identical to pi
+        self.image_data = self.colormap(self.data)
+        self.dataChanged.emit(self.image_data)
+
+    @PyQt6.QtCore.pyqtProperty(int, constant=False, notify=configChanged)
+    def delaySize(self):
+        return self._delay_size
+
+    @PyQt6.QtCore.pyqtProperty(int, constant=False, notify=configChanged)
+    def angleSize(self):
+        return self._angle_size
+
+    @PyQt6.QtCore.pyqtProperty(int, constant=False, notify=configChanged)
+    def delayMin(self):
+        return self.appconfig.get("delay_min")
+
+    @PyQt6.QtCore.pyqtProperty(int, constant=False, notify=configChanged)
+    def delayMax(self):
+        return self.appconfig.get("delay_max")
+
+app = AzimuthDelayApp(sys.argv)
 sys.exit(app.exec())
--- demos/azimuth-delay/azimuth-delay.qml
+++ demos/azimuth-delay/azimuth-delay.qml
@@ -1,145 +1,223 @@
-import QtQuick
-import QtQuick.Window
+import QtQuick.Controls.Material
+import QtQuick.Controls
+import QtQuick.Layouts
 import QtQuick.Shapes
+import QtQuick
 
-Window {
-    width: 1600
-    height: 800
-    visible: true
-    color: "lightgray"
+import "../common" as Common
 
-Rectangle{
-    id:plotArea
-    height:0.8*Math.min(parent.height, parent.width*0.4)
-    width: 2*height
-    anchors.centerIn: parent
-    color:"lightgray"
-    property color lineColor: Qt.rgba(0.3,0.3,0.3,0.9)
-    property real lineWidth: 2.5
+Common.ESPARGOSApplication {
+    id: window
+    title: "Azimuth-Delay Demo"
+    minimumWidth: 1600
+    minimumHeight: 800
 
-    ShaderEffect{
-        id: shader
-        anchors.fill: parent
-        Canvas {
-            id: plotCanvas
-            width: angle_size
-            height: delay_size
-            property var imageData: undefined
-            function createImageData() {
-                const ctx = plotCanvas.getContext("2d");
-                imageData = ctx.createImageData(width, height);
+    appDrawerComponent: Component {
+        Common.AppDrawer {
+            id: appDrawer
+            title: "Settings"
+            endpoint: appconfig
+
+            Label { Layout.columnSpan: 2; text: "Display Settings"; color: "#9fb3c8" }
+
+            Label { text: "Delay Min"; color: "#ffffff"; horizontalAlignment: Text.AlignRight; Layout.alignment: Qt.AlignRight; Layout.fillWidth: true }
+            SpinBox {
+                id: delayMinSpinBox
+                property string configKey: "delay_min"
+                property string configProp: "value"
+                Component.onCompleted: appDrawer.configManager.register(this)
+                onValueChanged: appDrawer.configManager.onControlChanged(this)
+                implicitWidth: 160
+                from: -10
+                to: 19
+                value: -3
             }
-            
-            onAvailableChanged: if(available) createImageData();
 
-            onPaint: {
-                if(plotCanvas.imageData){
+            Label { text: "Delay Max"; color: "#ffffff"; horizontalAlignment: Text.AlignRight; Layout.alignment: Qt.AlignRight; Layout.fillWidth: true }
+            SpinBox {
+                id: delayMaxSpinBox
+                property string configKey: "delay_max"
+                property string configProp: "value"
+                Component.onCompleted: appDrawer.configManager.register(this)
+                onValueChanged: appDrawer.configManager.onControlChanged(this)
+                implicitWidth: 160
+                from: -9
+                to: 20
+                value: 5
+            }
+
+            Common.GenericAppSettings {
+                id: genericAppSettings
+                insertBefore: genericAppSettingsAnchor
+            }
+
+            Item {
+                id: genericAppSettingsAnchor
+                Layout.columnSpan: 2
+                width: 0
+                height: 0
+                visible: false
+            }
+
+            Common.BacklogSettings {
+                id: backlogSettings
+                insertBefore: backlogSettingsAnchor
+            }
+
+            Item {
+                id: backlogSettingsAnchor
+                Layout.columnSpan: 2
+                width: 0
+                height: 0
+                visible: false
+            }
+
+            // Spacer
+            Rectangle {
+                id: endSpacer
+                Layout.columnSpan: 2
+                width: 1; height: 30
+                color: "transparent"
+            }
+        }
+    }
+
+    Rectangle {
+        id: plotArea
+        height: 0.8 * Math.min(parent.height, parent.width * 0.4)
+        width: 2 * height
+        anchors.centerIn: parent
+        color: "#11191e"
+        property color lineColor: Qt.rgba(0.3, 0.3, 0.3, 0.9)
+        property real lineWidth: 2.5
+
+        ShaderEffect {
+            id: shader
+            anchors.fill: parent
+            Canvas {
+                id: plotCanvas
+                width: backend.angleSize
+                height: backend.delaySize
+                property var imageData: undefined
+                function createImageData() {
                     const ctx = plotCanvas.getContext("2d");
-                    ctx.drawImage(plotCanvas.imageData, 0, 0);
+                    imageData = ctx.createImageData(width, height);
+                }
+
+                onAvailableChanged: if (available) createImageData();
+
+                onPaint: {
+                    if (plotCanvas.imageData) {
+                        const ctx = plotCanvas.getContext("2d");
+                        ctx.drawImage(plotCanvas.imageData, 0, 0);
+                    }
                 }
             }
+            property variant source: ShaderEffectSource {
+                sourceItem: plotCanvas
+                hideSource: true
+                smooth: true
+            }
 
+            vertexShader: "vertex_shader.qsb"
+            fragmentShader: "fragment_shader.qsb"
         }
-        property variant source: ShaderEffectSource {
-            sourceItem: plotCanvas
-            hideSource:true
-            smooth:true
-        }
-
-        vertexShader: "vertex_shader.qsb"
-        fragmentShader: "fragment_shader.qsb"
     }
-}
 
     Canvas {
         id: canvas
-        anchors.fill: parent        
-        property var origin_x:0.5*canvas.width
-        property var origin_y:0.5*(canvas.height-plotArea.height)+plotArea.height
+        anchors.fill: parent
+        property var origin_x: 0.5 * canvas.width
+        property var origin_y: 0.5 * (canvas.height - plotArea.height) + plotArea.height
         property var phi_num: 6
-        property var text_color: "black"
+        property var text_color: "white"
         property var text_width: 1
         property var text_font: "14px sans-serif"
-        property var delay_min: Delay_min
-        property var delay_max: Delay_max
-        property var num_delay: delay_max-delay_min
+        property var delay_min: backend.delayMin
+        property var delay_max: backend.delayMax
+        property var num_delay: delay_max - delay_min
 
         onPaint: {
             var context = getContext("2d");
-            context.font = text_font
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.font = text_font;
             context.fillStyle = text_color;
             context.lineWidth = text_width;
             context.textAlign = "right";
             context.textBaseline = "top";
-            context.fillText(delay_min, origin_x, origin_y+5)
-            for(let i = 0; i<num_delay; i++){
-                context.beginPath()
+            context.fillText(delay_min, origin_x, origin_y + 5);
+            for (let i = 0; i < num_delay; i++) {
+                context.beginPath();
                 context.strokeStyle = plotArea.lineColor;
                 context.lineWidth = plotArea.lineWidth;
-                context.arc(origin_x, origin_y, (1-(1/num_delay)*i)*plotArea.height,Math.PI,2*Math.PI);
+                context.arc(origin_x, origin_y, (1 - (1 / num_delay) * i) * plotArea.height, Math.PI, 2 * Math.PI);
                 context.stroke();
                 context.fillStyle = text_color;
                 context.lineWidth = text_width;
                 context.textAlign = "right";
                 context.textBaseline = "top";
-                context.fillText(((delay_max-i)), origin_x+(1-(1/num_delay)*i)*plotArea.height, origin_y+5);
+                context.fillText(((delay_max - i)), origin_x + (1 - (1 / num_delay) * i) * plotArea.height, origin_y + 5);
             }
-            for(let i =-phi_num/2; i<=phi_num/2; i++){
-                context.beginPath()
+            for (let i = -phi_num / 2; i <= phi_num / 2; i++) {
+                context.beginPath();
                 context.strokeStyle = plotArea.lineColor;
                 context.lineWidth = plotArea.lineWidth;
                 context.moveTo(origin_x, origin_y);
-                context.lineTo(origin_x+Math.sin(i/phi_num*Math.PI)*plotArea.height, origin_y-Math.cos(i/phi_num*Math.PI)*plotArea.height);
+                context.lineTo(origin_x + Math.sin(i / phi_num * Math.PI) * plotArea.height, origin_y - Math.cos(i / phi_num * Math.PI) * plotArea.height);
                 context.stroke();
-                if(i<0){
+                if (i < 0) {
                     context.fillStyle = text_color;
                     context.lineWidth = text_width;
                     context.textAlign = "right";
                     context.textBaseline = "bottom";
-                    context.fillText(180/phi_num*(-i)+"°", origin_x+Math.sin(i/phi_num*Math.PI)*(plotArea.height+5), origin_y-Math.cos(i/phi_num*Math.PI)*(plotArea.height+5));
-                }
-                else if(i == 0){
+                    context.fillText(180 / phi_num * (-i) + "°", origin_x + Math.sin(i / phi_num * Math.PI) * (plotArea.height + 5), origin_y - Math.cos(i / phi_num * Math.PI) * (plotArea.height + 5));
+                } else if (i == 0) {
                     context.fillStyle = text_color;
                     context.lineWidth = text_width;
                     context.textAlign = "center";
                     context.textBaseline = "bottom";
-                    context.fillText(0+"°", origin_x+Math.sin(i/phi_num*Math.PI)*(plotArea.height+5), origin_y-Math.cos(i/phi_num*Math.PI)*(plotArea.height+5));
-                }
-                else{
+                    context.fillText(0 + "°", origin_x + Math.sin(i / phi_num * Math.PI) * (plotArea.height + 5), origin_y - Math.cos(i / phi_num * Math.PI) * (plotArea.height + 5));
+                } else {
                     context.fillStyle = text_color;
                     context.lineWidth = text_width;
                     context.textAlign = "left";
                     context.textBaseline = "bottom";
-                    context.fillText(180/phi_num*(-i)+"°", origin_x+Math.sin(i/phi_num*Math.PI)*(plotArea.height+5), origin_y-Math.cos(i/phi_num*Math.PI)*(plotArea.height+5));
+                    context.fillText(180 / phi_num * (-i) + "°", origin_x + Math.sin(i / phi_num * Math.PI) * (plotArea.height + 5), origin_y - Math.cos(i / phi_num * Math.PI) * (plotArea.height + 5));
                 }
             }
         }
 
+        Connections {
+            target: backend
+            function onConfigChanged() {
+                canvas.requestPaint();
+            }
+        }
     }
 
-    Timer{
-        interval: 1000/60
-        running: true
+    Timer {
+        interval: 1000 / 60
+        running: !backend.initializing
         repeat: true
         onTriggered: {
-        backend.update_data();
+            backend.update_data();
         }
     }
 
     Connections {
         target: backend
         function onDataChanged(image_data) {
-            if (plotCanvas.imageData === undefined){
-                const ctx = plotCanvas.getContext("2d")
-                plotCanvas.imageData = ctx.createImageData(angle_size, delay_size)
+            if (plotCanvas.imageData === undefined || image_data.length !== plotCanvas.imageData.data.length) {
+                const ctx = plotCanvas.getContext("2d");
+                plotCanvas.imageData = ctx.createImageData(backend.angleSize, backend.delaySize);
             }
+            
             let len = plotCanvas.imageData.data.length;
-            for (let i = 0; i < len; i++){
+            for (let i = 0; i < len; i++) {
                 plotCanvas.imageData.data[i] = image_data[i];
             }
             plotCanvas.requestPaint();
         }
     }
-
 }
 
